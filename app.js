b4c2d3bc6f286fb877cc3d286a801b894c06dfc4
Kenneth14031129 <kenneth14031129@gmail.com>
2024-11-05 21:50:47 +0800

    Update for full-stack deployment

--- app.js
+++ app.js
@@ -45,8 +45,10 @@ app.use("/api/v1/auth", authRoute);
 
 // Serve static files in production
 if (process.env.NODE_ENV === 'production') {
+    // Serve static files from the React app
     app.use(express.static(path.join(__dirname, 'client/dist')));
     
+    // Handle React routing, return all requests to React app
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, 'client/dist', 'index.html'));
     });
